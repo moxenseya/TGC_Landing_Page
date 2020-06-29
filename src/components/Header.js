26cbaf208d893f5e6a93f65f0bff2b5f81271e55
Abrar Hussain Syed <keyblademaster1000@gmail.com>
2020-06-28 20:16:00 -0400

    Navbar updated, added BG placeholder, test with sample header

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,18 +11,18 @@ export default class Header extends Component {
           <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
             <ul id="nav" className="nav">
                <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-               <li><a className="smoothscroll" href="#about">About</a></li>
-             <li><a className="smoothscroll" href="#resume">Resume</a></li>
-               <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-               <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
+               <li><a className="smoothscroll" href="#about">The Team</a></li>
+             <li><a className="smoothscroll" href="#resume">Mission</a></li>
+               <li><a className="smoothscroll" href="#portfolio">How it Works</a></li>
+               <li><a className="smoothscroll" href="#testimonials">Gallery</a></li>
                <li><a className="smoothscroll" href="#contact">Contact</a></li>
             </ul>
          </nav>
 
          <div className="row banner">
             <div className="banner-text">
-               <h1 className="responsive-headline">I am {resumeData.name}.</h1>
-               <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>I am a {resumeData.role}.{resumeData.roleDescription}
+               <h1 className="responsive-headline">{resumeData.name}</h1>
+               <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>{resumeData.role}.{resumeData.roleDescription}
                </h3>
                <hr/>
                <ul className="social">
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,10 +1,10 @@
 let resumeData = {
     "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
+    "name": "The Green Company",
+    "role": "Go Green, Get Green",
     "linkedinId":"Your LinkedIn Id",
     "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "roleDescription": "The Green Company is an initiative to protect our environment via efficient waste recycling through technology.",
     "socialLinks":[
         {
           "name":"linkedin",
@@ -22,8 +22,8 @@ let resumeData = {
           "className":"fa fa-twitter"
         }
       ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
+    "aboutme":"Detailed Information goes here",
+    "address":"New York, USA",
     "website":"https://rbhatia46.github.io",
     "education":[
       {
